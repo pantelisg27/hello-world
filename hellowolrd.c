f4d00898a0c0b78ec9d3dd382c249b555cf029fb
pantelisg27 <pgiotis27@yahoo.com>
2024-09-30 23:56:40 +0300

    Add files via upload

---
+++ hellowolrd.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello World !\n");
+    int i;
+    for (i=0;i<1000;i++)
+    {
+        printf("Hello World !\n");
+    }
+    
+
+
+
+
+
+
+    return 0;
+}
